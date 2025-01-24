cb7be2e69ade2bf376a645244e02ef0982bf5e7d
Gerson Ramirez <ram22281@uvg.edu.gt>
2025-01-23 19:30:17 -0600

    feat: implement correct usage of buffer and clean code

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 def cargar_buffer(entrada, inicio, tamano_buffer):
-  buffer = entrada[inicio:inicio + tamano_buffer]
-  if len(buffer) < tamano_buffer:
-    buffer.append("eof")
-  return buffer
+    buffer = entrada[inicio:inicio + tamano_buffer]
+    if len(buffer) < tamano_buffer:
+        buffer.append("eof")
+    return buffer
 
-def procesar_buffer(buffer):
-    temp = []
+def procesar_buffer(buffer, temp):
+    lexemas = []
     i = 0
-    while i <= len(buffer):
+    while i < len(buffer):
         if buffer[i] == " ":
-            print(f'Lexema procesado: {"".join(temp)}')
-            buffer.pop(i)
+            lexemas.append("".join(temp))
             temp.clear()
         elif buffer[i] == "eof":
-            print(f'Lexema procesado: {"".join(temp)}')
+            if temp:
+                lexemas.append("".join(temp))
             break
-        temp.append(buffer.pop(i))
-       
+        else:
+            temp.append(buffer[i])
+        i += 1
+    return lexemas, temp
 
-entrada = list("Esto es un ejemplo de entrada con varios lexemas")
+entrada = list("Esto es un ejemplo de entrada con eof")
 inicio = 0
 tamano_buffer = 10
-buffer = cargar_buffer(entrada, inicio, tamano_buffer)
-procesar_buffer(buffer)
+temp = []
+
+while inicio < len(entrada):
+    buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+    lexemas, temp = procesar_buffer(buffer, temp)
+    for lexema in lexemas:
+        print(f'Lexema procesado: {lexema}')
+    inicio += tamano_buffer
